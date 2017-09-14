bfc76dd2b2b6ed05af91f29f394c6541b934deeb
Anton Kosterev <kosterevanton@gmail.com>
2017-09-14 20:55:05 +0300

    v0.001

---
+++ Assets/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(10,0,0);
+		Debug.Log("Test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
